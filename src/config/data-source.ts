97a2d0d8bb22d989c7407b90d1d5b8b7383b3dea
Coderman47 <askinglucho@gmail.com>
2025-02-25 00:00:28 -0300

    add verify connection to postgres before try to connect with typeorm

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -1,9 +1,5 @@
 import { DataSource } from "typeorm";
-// import { Pet } from "../Model/Pet";
-// import { User } from "../Model/User";
-// import { Product } from "../Model/Product";
-// import { Veterinary } from "../Model/Veterinary";
-import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from "dotenv";
 dotenv.config();
 
 const isTestEnv = process.env.NODE_ENV === 'test';
@@ -18,15 +14,6 @@ export default new DataSource({
 	synchronize: isTestEnv, // Solo sincroniza en tests para evitar problemas en producción
 	logging: false,
 	entities: ["dist/Model/*.ts"],
-	// migrations: ["dist/migrations/*.ts"],
-	// subscribers: ["dist/subscribers/*.ts"],
+	migrations: ["dist/migrations/*.ts"],
+	subscribers: ["dist/subscribers/*.ts"],
   });
-
-// export default new DataSource({
-// 	name: "default",
-// 	type: "postgres",
-// 	url: process.env.DB_DEPLOY,
-// 	entities: [Pet, User, Product, Veterinary],
-// 	synchronize: true,
-// 	logging: false,
-// });
