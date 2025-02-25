79b2d7de13c1d646c3a62f06fb298bbc908aa3c6
Coderman47 <askinglucho@gmail.com>
2025-02-25 17:07:04 -0300

    add port and host env variables

--- src/config/data-source.ts
+++ src/config/data-source.ts
@@ -6,14 +6,14 @@ const isTestEnv = process.env.NODE_ENV === 'test';
 
 export default new DataSource({
 	type: "postgres",
-	host: process.env.DB_HOST,
+	host: process.env.DB_HOST || 'postgres',
 	port: Number(process.env.DB_PORT),
 	username: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: isTestEnv ? process.env.TEST_DB_NAME : process.env.DB_NAME,
 	synchronize: isTestEnv,
 	logging: false,
-	entities: ["dist/Model/*.ts"],
-	migrations: ["dist/migrations/*.ts"],
-	subscribers: ["dist/subscribers/*.ts"],
+	entities: ["src/Model/*.ts"],
+	migrations: ["src/migrations/*.ts"],
+	subscribers: ["src/subscribers/*.ts"],
   });
