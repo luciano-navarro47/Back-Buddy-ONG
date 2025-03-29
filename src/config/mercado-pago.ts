fdb1587bde0b942958f0d9222b837a1189ef60c2
Coderman47 <askinglucho@gmail.com>
2025-03-29 09:08:07 -0300

    fix name reference env variable in mercado-pago file

--- src/config/mercado-pago.ts
+++ src/config/mercado-pago.ts
@@ -2,12 +2,12 @@ import { MercadoPagoConfig } from "mercadopago";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-if (!process.env.MP_ACCESS_TOKEN) {
+if (!process.env.ACCESS_TOKEN_MP) {
     throw new Error('MP_ACCESS_TOKEN is required');
   }
 
 const mercadoPagoClient = new MercadoPagoConfig({ 
-    accessToken: process.env.MP_ACCESS_TOKEN 
+    accessToken: process.env.ACCESS_TOKEN_MP
 });
 
 export default mercadoPagoClient;
