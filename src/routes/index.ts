54bf504589370f2cf9382e62b3047b28fc7c75f7
Coderman47 <askinglucho@gmail.com>
2025-10-01 20:48:32 -0300

    create: process-payment routes and update mercadopago sdk

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -7,16 +7,18 @@ import donationRouter from "./donation.route";
 import subscriptionRouter from "./subscription.route";
 import loginRouter from "./login.router";
 import webhookRouter from "./webhook.routes";
+import processPaymentRouter from "./process-payment.route";
 
 const router = Router();
 
+router.use("/donation", donationRouter);
 router.use("/login", loginRouter);
 router.use("/pet", petsRouter);
-router.use("/user", userRouter);
+router.use("process-payment", processPaymentRouter)
 router.use("/products", productRouter);
-router.use("/veterinary", veterinaryRouter);
-router.use("/donation", donationRouter);
 router.use("/subscription", subscriptionRouter);
+router.use("/user", userRouter);
+router.use("/veterinary", veterinaryRouter);
 router.use("/webhooks", webhookRouter);
 
 export default router;
---
+++ src/routes/process-payment.route.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+// import { a } from "a";
+
+const processPaymentRouter = Router();
+
+processPaymentRouter.get("/", );
+processPaymentRouter.post("/", );
+processPaymentRouter.put("/:id", );
+processPaymentRouter.delete("/:id", );
+
+export default processPaymentRouter;
